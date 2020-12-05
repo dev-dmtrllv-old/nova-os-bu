64503d02716230a9a7f3e518362a247457e94a60
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-12-05 02:15:15 +0100

    can load files now :D

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -46,25 +46,16 @@ boot_start:
 	mov byte [drive], dl		; store the booted drive number
 
 	call cls
-
 	mov si, msg_loading
 	call print_line
 
-
-	; REMOVED BECAUSE OF THERE IS NOT ENOUGH SPACE IN 512 BYTES
-	; check if extended drive bios interrupts are available
-	; mov ah, 0x41
-	; mov bx, 0x55aa
-	; mov dx, word [drive]
-	; jc err_extended_drive_ext
-
-
-; LOAD_ROOT: 
+	; LOAD_ROOT 
 	; store size of root directory into disk address packets -> sectors_number (the number of sectors to loader)
     mov ax, 0x0020                      ; 32 byte directory entry
     mul word [rootDirEntries]           ; total size of directory
     div word [bytesPerSector]        	; sectors used by directory
-    mov word [dap_sectors_number], ax
+    mov word [dap_sectors_number], ax 	; set number of sectors in dap
+	xchg ax, cx							; set cx to root dir size (in sectors)
           
 	; store location of root directory into disk adress packet -> lba
 	mov al, byte [numberOfFats]
@@ -72,6 +63,7 @@ boot_start:
 	add ax, word [reservedSectors]
 	mov word [datasector], ax
 	add word [datasector], cx
+	mov word [debug], cx
 	mov word [dap_lba], ax
 
 	; setup destination to load root directory in 0x0000:0x7e00
@@ -81,7 +73,7 @@ boot_start:
 
 	call read_sectors
 
-	; browse root directory for binary image
+ 	; browse root directory for binary image
 	mov cx, [rootDirEntries]
 	mov di, 0x7e00
 .find_image_loop:
@@ -98,35 +90,45 @@ boot_start:
 	jmp err_image_missing
 
 found_image:
-	mov dx, [di + 0x001a]
-	mov word [image_cluster], dx 			; store image cluster number
+	mov dx, [di + 0x001a] 			; get byte 26 for the cluster number
+	mov word [image_cluster], dx 	; save cluster into image_cluster
 
-	; read FAT into 0x8a00
-	xor	ax, ax
-	mov	al, BYTE [numberOfFats]          	; number of FATs
-	mul	WORD [sectorsPerFat]             	; sectors used by FATs
-	mov	[dap_sectors_number], ax			
+; load fat
+	mov cx, word [sectorsPerFat]
+	mov ax, 0x4
+	mov word [dap_sectors_number], ax
+	mov ax, word [reservedSectors]
+	mov word [dap_lba], ax
+	xor bx, bx
+	mov es, bx
+	mov bx, 0x7e00
+	call read_sectors
 
-	mov ax, WORD [reservedSectors]
-	mov [dap_lba], ax
+; load image
 
+load_img_cluster:
+	mov ax, [image_cluster]				; cluster to read
+    call cluster_to_lba					
+	mov word [dap_lba], ax
 	xor bx, bx
 	mov es, bx
 	mov bx, 0x8a00
 
-	call read_sectors
+	mov cx, word [sectorsPerCluster]
+.loop_load_img:
+	call read_sectors                    ; read in cluster
 
-load_image:
+	add word [dap_lba], 1
+	add bx, 0x200
+	
+	loop .loop_load_img
 
-
+; check which cluster to load next (or stop loading if image_cluster >= 0xfff8)
+	; get values at cluster position from FAT
+	
+	
 	jmp halt_cpu
 	
-;  errors
-; err_extended_drive_ext:
-; 	mov si, msg_extended_drive_err
-; 	call print_line
-; 	jmp halt_cpu
-
 err_read_drive_sect:
 	mov si, msg_read_drive_err
 	call print_line
@@ -173,17 +175,28 @@ cls:
 	popa
     ret
 
-read_sectors:
+
+read_sectors: ; es:bx read location
+	pusha
+	mov word [dap_buf_off], bx
 	xor ax, ax
 	mov ah, 0x42
-	mov dx, word [drive]
+	xor dx, dx
+	mov dl, byte [drive]
 	mov si, dap
 	int 0x13
+	popa
 	jc err_read_drive_sect
 	ret
 
-read_file:
-	; TODO write routine
+cluster_to_lba: ; (ax) lba = (cluster_num - 2) * num_of_sect_in_cluster
+	push cx
+	sub ax, 0x0002
+	xor cx, cx
+	mov cl, byte [sectorsPerCluster]
+	mul cx
+	add ax, WORD [datasector]
+	pop cx
 	ret
 
 ;-----------;
@@ -193,24 +206,23 @@ read_file:
 ; Disk address packet
 dap:
 dap_packet_size: 		db 0x10 		; packet size
-dap_reserved:   		db 0x0			; 0
-dap_sectors_number:  	dw 0x1			; sectors to load
+dap_reserved:   		db 0x00			; 0
+dap_sectors_number:  	dw 0x01			; sectors to load
 dap_buf_off:    		dw 0x7e00		; offset where to load
 dap_buf_seg:    		dw 0x0000		; segment where to load
 dap_lba:        		dd 0x1			; from where to load lba
             			dd 0x0
 
-
 datasector				dw 0x0000
 image_cluster			dw 0x0000
-
-image_name				dw "NOVALDR SYS", 0
-
+debug					dw 0x0000
+image_name				dw "TEST    TXT", 0
 msg_loading				db "Loading Boot Image ", 0
-; msg_extended_drive_err	db "Error extended drive not supported by bios!", 0
 msg_read_drive_err		db "Error reading sector!", 0
 msg_image_missing		db "Missing Image!", 0
 msg_EOL					db 13, 10, 0
+
+%include "src/boot/fs.asm"
 
 ;---------------------------;
 ;	PAD AND BOOT SIGNATURE	;
